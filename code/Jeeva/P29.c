e46de7b4fa31b192758089c82870b4f95a129e32
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-01-21 18:57:22 +0530

    Update P29.c

--- code/Jeeva/P29.c
+++ code/Jeeva/P29.c
@@ -2,11 +2,11 @@
 #include<stdio.h>
 int main()
 {
-int t=0;
+int time=0;
 int min=0;
 int hour=0;
-scanf("%d",&t);
-min=t%60;
-hour=(t-min)/60;
+scanf("%d",&time);
+min=time%60;
+hour=(time-min)/60;
 printf("%d %d ",hour,min);
 }
