bdacf85017902d02075790e2b3f02bf7fae1d9d4
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-02-02 16:27:09 +0530

    Create p42.c

---
+++ code/Jeeva/p42.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,str1,str2;
+	char a[100],b[100];
+	printf("enter the first string\n");
+	printf("enter the second string\n");
+	scanf("%s %s",&a,&b);
+	i=strlen(a);
+	j=strlen(b);
+	if(a==b)
+	{
+		printf(" %s\n",a );
+	}else if(a>=b)
+	{
+		printf(" %s\n",a);
+	}else
+	{
+		printf(" %s \n",b);
+	}
+	return 0;
+}
