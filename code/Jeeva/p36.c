63734622c788e84682cac906609ad72d6940faed
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-02-07 16:59:07 +0530

    Update p36.c

--- code/Jeeva/p36.c
+++ code/Jeeva/p36.c
@@ -2,7 +2,7 @@
 int main()
 {
 	char s[60];
-	int i=0,a=0,b=0,k=0;
+	int i=0,a=0,b=0,c=0;
 	printf("enter the input\n");
 	gets(s);
 	while(s[i]!='\0')
@@ -17,7 +17,7 @@ int main()
 	}
 	else
 	{
-		k++;
+		c++;
 	}
 	i++;
 	}
