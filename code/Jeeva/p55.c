db6fbbd1da97d789f27ebc03cd930db06774a49f
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-02-07 16:27:49 +0530

    Create p55.c

---
+++ code/Jeeva/p55.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int n1,n2,a;
+	printf("enter the numbers\n");
+	scanf("%d %d",&n1,&n2);
+	a=n1*n2;
+	if(a%2==0)
+	
+		printf("even");
+	
+		else
+		printf("odd");
+	
+	return 0;
+}
