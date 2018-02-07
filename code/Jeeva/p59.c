fd298243501845bd7c9ff069948a8d823554afb9
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-02-07 16:54:49 +0530

    Update p59.c

--- code/Jeeva/p59.c
+++ code/Jeeva/p59.c
@@ -2,20 +2,20 @@
 int main()
 {
 	int i;
-	int a[10],max;
+	int a[10],m;
 	printf("enter the number\n");
 	for(i=0;i<10;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	max=a[0];
+	m=a[0];
 	for(i=0;i<10;i++)
 	{
-		if(a[i]>max)
+		if(a[i]>m)
 		{
-			max=a[i];
+			m=a[i];
 		}
 	}
-printf("maximum number is %d",max);
+printf("maximum number is %d",m);
 return 0;
 }
