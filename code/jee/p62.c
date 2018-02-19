56ec32d234d963a4b3fe3bb5857e3fc234a44e36
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-02-19 16:21:22 +0530

    Create p62.c

---
+++ code/jee/p62.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	int a[199],i,flag=0;
+	printf("enter the string\n");
+	scanf("%s",&a[i]);
+	for(i=0;i<a[i];i++)
+	{
+		if((a[i]=='0')||(a[i]=='1'))
+		
+		{
+		flag=1;
+		break;
+		}
+		else
+		{
+			flag=0;
+			break;
+		}
+	
+}
+if((a[i]=='0')||(a[i]=='1'))
+
+	printf("yes");
+
+else
+{
+
+	printf("no");
+	
+}
+return 0;
+}
