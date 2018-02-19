ca93d07458868bff37c68e1c9c4a02775ab7584a
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-02-19 16:49:47 +0530

    Create p61.c

---
+++ code/jee/p61.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+	char a[100];
+	int i,n;
+	printf("enter the string\n");
+	scanf("%s",&a[i]);
+	printf("output is:\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("%c",a[i]);
+	}
+	
+}
