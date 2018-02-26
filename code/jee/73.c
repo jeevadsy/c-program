7e53965caa458210884190b79fb7fe49a52833ff
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-02-26 16:45:27 +0530

    Create 73.c

---
+++ code/jee/73.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("enter the numbers\n");
+	scanf("%d",&a);
+	printf("enter the first number\n");
+	scanf("%d",&b);
+    printf("enter the last number\n");
+	scanf("%d",&c);
+	if((a>=b)&&(a<=c))
+	
+	
+		
+		printf("yes");
+			
+		else
+		printf("no");
+	
+	return 0;
+}
