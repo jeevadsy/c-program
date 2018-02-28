a8c278b6d9abcaec28dffe74b247101553abecad
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-02-28 16:48:28 +0530

    Rename p80.c to p80a.c

---
+++ code/jee/p80a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int ar[100],i,n;
+	printf("enter the number");
+	scanf("%d",&n);
+	printf("enter the number");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&ar[i]);
+	}
+	for(i=0;ar[i]<=n;i++)
+	{
+	if(ar[i]%2!=0)
+	{
+		printf("%d\t",ar[i]);
+	}
+		
+	}
+
+	return 0;
+}
+
