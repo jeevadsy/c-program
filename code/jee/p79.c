6689ae3273a50a72142f516599c1b1c2f098b641
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-03-02 16:25:35 +0530

    Update p79.c

--- code/jee/p79.c
+++ code/jee/p79.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c,d,i,flag=0;
+	int a,b,c;
 	printf("enter the numbers\n");
 	scanf("%d %d",&a,&b);
 	c=a*b;
-	for(i=1;i<=c/2;i++)
-	{
-		d=i*i;
-		if(d==c)
-		{
-		flag=1;
-		break;
-		}
-
-	}
-if(flag)
-printf("yes");
-	else
-	printf("no");
-	return 0;
+	printf("%d\n",c);
+	if(a==b)
+	
+		printf("yes");
+		else
+		printf("no");
+	
+	
+return 0;
 }
