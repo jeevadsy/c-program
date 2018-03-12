07ff958424d5b0d2bfb0f082af01659c04f3619c
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-03-12 16:40:24 +0530

    Create p87.c

---
+++ code/jee/p87.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+ 
+int main() 
+{
+int a,i,c,b;
+printf("enter the numbers ");
+scanf("%d %d",&a,&b);
+for(i=1;i<a;i++)
+{
+	if((a%i==0)&&(b%i==0))
+	{
+		c=i;
+	}
+}
+printf("%d",c);
+	return 0;
+}  
