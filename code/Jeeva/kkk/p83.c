965dcaa90d8c5aefc05d76e9ae7061f7cb1c946b
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-03-14 15:58:56 +0530

    Update p83.c

--- code/Jeeva/kkk/p83.c
+++ code/Jeeva/kkk/p83.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main(void) {
 int a[10],b[10],i,n;
-char c[10];
+char s[10];
 printf("\n enter the number of sets");
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
-	scanf("%d %c %d",&a[i],&c[i],&b[i]);
+	scanf("%d %c %d",&a[i],&s[i],&b[i]);
 }
 for(i=0;a[i]!='\0';i++)
 {
-	if(c[i]=='/')
+	if(s[i]=='/')
 	{
 		printf("\n%d is the division value\n",a[i]/b[i]);
 	}
-	if(c[i]=='%')
+	if(s[i]=='%')
 	{
 		printf("%d is the modulo value\n",a[i]%b[i]);
 	}
