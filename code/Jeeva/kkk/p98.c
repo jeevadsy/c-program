d8af75df948e849080d96e7dadb05c24f88a14d5
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-03-16 16:42:48 +0530

    Create p98.c

---
+++ code/Jeeva/kkk/p98.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int n,a[100],i=0,j=0,count=0,max;
+printf("enter the numbers:\n");
+scanf("%d",&n);
+printf("enter the numbers:\n");
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+if(a[j]>a[i])
+  {
+    a[i]=a[j];
+    ++count;
+  }
+		}
+}
+printf("%d",count);
+return 0;
+}
