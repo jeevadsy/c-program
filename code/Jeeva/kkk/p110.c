e8077c27045fc92e266f00ce55b68365c647e23c
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-03-19 15:55:23 +0530

    Create p110.c

---
+++ code/Jeeva/kkk/p110.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() 
+{
+ int sum=0,n,i;
+ printf("enter the number\n");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+ sum=sum+i;
+ }
+ printf("the sum first n number is %d",sum);
+  return 0;
+}
