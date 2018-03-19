1dd33b9075a1f73cd485a226e8636243d6910de2
jeevadsy <35127996+jeevadsy@users.noreply.github.com>
2018-03-19 17:00:09 +0530

    Create p999.c

---
+++ code/Jeeva/kkk/p999.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+int main() 
+{
+ int a,n,i;
+ printf("enter the number \n");
+ scanf("%d ",&n);
+ if(n==1)
+ {
+ 	printf("A");
+ }
+  if(n==2)
+ {
+ 	printf("B");
+ }
+  if(n==3)
+ {
+ 	printf("C");
+ }
+  if(n==4)
+ {
+ 	printf("D");
+ }
+  if(n==5)
+ {
+ 	printf("E");
+ }
+  if(n==6)
+ {
+ 	printf("F");
+ }
+  if(n==7)
+ {
+ 	printf("G");
+ } if(n==8)
+ {
+ 	printf("H");
+ } if(n==9)
+ {
+ 	printf("I");
+ } if(n==10)
+ {
+ 	printf("J");
+ } if(n==11)
+ {
+ 	printf("K");
+ } if(n==12)
+ {
+ 	printf("L");
+ } if(n==13)
+ {
+ 	printf("M");
+ } if(n==14)
+ {
+ 	printf("N");
+ }
+  if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ } if(n==1)
+ {
+ 	printf("A");
+ }
+
+  return 0;
+}
